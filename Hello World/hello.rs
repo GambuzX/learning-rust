e91054618a44b64eaf8c927379a3f7b6a6da6ddc
GambuzX <mg_mesquita@hotmail.com>
2022-11-19 15:03:55 +0000

    display

--- Hello World/hello.rs
+++ Hello World/hello.rs
@@ -12,4 +12,57 @@ fn main() {
     // Print text to the console
     println!("Hello World!");
     println!("I'm a Rustacean!");
+
+    // format string
+    println!("{} days", 31);
+
+    // positional arguments
+    println!("{0}, this is {1}. {1} this is {0}", "mokey", "banana");
+
+    // named arguments
+    println!("{name} is a {thing}", name="George", thing="Clown");
+
+    // Different formatting can be invoked by specifying the format character after a `:`.
+    println!("Base 10:               {}",   69420); //69420
+    println!("Base 2 (binary):       {:b}", 69420); //10000111100101100
+    println!("Base 8 (octal):        {:o}", 69420); //207454
+    println!("Base 16 (hexadecimal): {:x}", 69420); //10f2c
+    println!("Base 16 (hexadecimal): {:X}", 69420); //10F2C
+
+    // You can right-justify text with a specified width. This will
+    // output "    1". (Four white spaces and a "1", for a total width of 5.)
+    println!("{number:>5}", number=1);
+
+    // You can pad numbers with extra zeroes,
+    //and left-adjust by flipping the sign. This will output "10000".
+    println!("{number:0<5}", number=1);
+
+    // You can use named arguments in the format specifier by appending a `$`
+    println!("{number:0>width$}", number=1, width=5);
+
+    // Rust even checks to make sure the correct number of arguments are
+    // used.
+    println!("My name is {0}, {1} {0}", "Bond", "James");
+    // FIXME ^ Add the missing argument: "James"
+
+    // Only types that implement fmt::Display can be formatted with `{}`. User-
+    // defined types do not implement fmt::Display by default
+
+    #[allow(dead_code)]
+    struct Structure(i32);
+
+    // This will not compile because `Structure` does not implement
+    // fmt::Display
+    // println!("This struct `{}` won't print...", Structure(3));
+    // TODO ^ Try uncommenting this line
+
+    // For Rust 1.58 and above, you can directly capture the argument from a
+    // surrounding variable. Just like the above, this will output
+    // "     1". 5 white spaces and a "1".
+    let number: f64 = 1.0;
+    let width: usize = 5;
+    println!("{number:>width$}");
+
+    let pi = 3.141592;
+    println!("Pi is roughly {:.3}", pi);
 }
